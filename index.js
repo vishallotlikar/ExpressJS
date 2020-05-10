4ebd9a6f5dbc166e83e19222909936478855b1b6
vishal lotlikar <lotlikar18@gmail.com>
2020-05-10 13:47:00 +0530

    Pug if else conditions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const courses = require('./router/courses');
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.get('/pug', function(req, res) {
+app.get('/pug', function (req, res) {
     res.render("example", {
         name: "Vishal Lotlikar",
         url: "https://www.google.com",
@@ -14,12 +14,22 @@ app.get('/pug', function(req, res) {
     });
 })
 
-function logInfo(req, res, next){
+app.get('/pug-page', function (req, res) {
+    var person = {
+        name: "Vishal Lotlikar",
+        city: "Panjim",
+    }
+    res.render("page", {
+        data: person
+    });
+})
+
+function logInfo(req, res, next) {
     console.log("Hello Vishal");
     next();
 };
 
-function addData(req, res, next){
+function addData(req, res, next) {
     var person = {
         name: "Vishal Lotlikar",
         city: "Panjim"
@@ -32,7 +42,7 @@ function addData(req, res, next){
 app.use(logInfo);
 app.use(addData);
 
-app.use('/user/:id([0-3]{1})', function (req, res, next){
+app.use('/user/:id([0-3]{1})', function (req, res, next) {
     var name = ['Sandeep', 'Amit', 'Deepak', 'Aman'];
     req.name = name[req.params.id];
     next();
@@ -42,29 +52,29 @@ app.get('/user/:id([0-3]{1})', function (req, res) {
     res.send('Name: ' + req.name);
 })
 
-app.get('/person', function(req, res) {
+app.get('/person', function (req, res) {
     res.send("Name: " + req.person.name + ", City: " + req.person.city)
 })
 
 app.use('/courses', courses)
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send("Hello from express.")
 })
 
-app.get('/profile/:id', function(req, res) {
+app.get('/profile/:id', function (req, res) {
     res.send("Profile ID: " + req.params.id);
 })
 
-app.get('/:course_name/courses/:id', function(req, res) {
+app.get('/:course_name/courses/:id', function (req, res) {
     res.send("Course Name: " + req.params.course_name + ", ID: " + req.params.id);
 })
 
-app.get('/message/:phone_number([0-9]{10})', function(req, res) {
+app.get('/message/:phone_number([0-9]{10})', function (req, res) {
     res.send("Message to " + req.params.phone_number);
 })
 
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
     res.send("Page Not Found.");
 })
 
